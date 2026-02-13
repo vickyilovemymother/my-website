8c92c0836a2331e6247f7170e62a92f686245981
Vigneshwaran Pugazhenthi <204408473+vickyilovemymother@users.noreply.github.com>
2026-02-14 04:24:47 +0530

    Rename import from 'three' to 'THREE'

--- vp-configurator/core/SceneManager.js
+++ vp-configurator/core/SceneManager.js
@@ -1,4 +1,4 @@
-import * as three from "https://cdn.jsdelivr.net/npm/three@0.160/build/three.module.js";
+import * as THREE from "https://cdn.jsdelivr.net/npm/three@0.160/build/three.module.js";
 import { OrbitControls } from "https://cdn.jsdelivr.net/npm/three@0.160/examples/jsm/controls/OrbitControls.js";
 
 export class SceneManager {
