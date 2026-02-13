f550c35ac2a0820e6b7e12c8805fb23ac49e9e3c
Vigneshwaran Pugazhenthi <204408473+vickyilovemymother@users.noreply.github.com>
2026-02-14 04:43:18 +0530

    Add StateManager and update mannequin handling

--- vp-configurator/main.js
+++ vp-configurator/main.js
@@ -1,23 +1,26 @@
 import { SceneManager } from "./core/SceneManager.js";
 import { EnvironmentManager } from "./core/EnvironmentManager.js";
 import { ModelLoader } from "./core/ModelLoader.js";
+import { StateManager } from "./core/StateManager.js";
 
 const loadingScreen = document.getElementById("loading-screen");
 
 const sceneManager = new SceneManager();
 const environmentManager = new EnvironmentManager(sceneManager);
 const modelLoader = new ModelLoader();
+const stateManager = new StateManager();
 
 async function init() {
   await environmentManager.loadHDR(
-    "./vp-configurator/assets/hdr/HC_VP.hdr"
+    "/vp-configurator/assets/hdr/HC_VP.hdr"
   );
 
   const mannequin = await modelLoader.loadModel(
-    "./vp-configurator/assets/mannequin/Men_Mannequin.glb"
+    "/vp-configurator/assets/mannequin/Men_Mannequin.glb"
   );
 
   sceneManager.add(mannequin);
+  stateManager.setMannequin(mannequin);
 
   sceneManager.start();
 
