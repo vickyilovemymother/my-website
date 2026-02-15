00082f9db1d1e56f9de769bd7bdeae925eef7e8f
Vigneshwaran Pugazhenthi <204408473+vickyilovemymother@users.noreply.github.com>
2026-02-16 00:40:28 +0530

    Update SceneManager.js

--- vp-configurator/core/SceneManager.js
+++ vp-configurator/core/SceneManager.js
@@ -58,23 +58,4 @@ export class SceneManager {
     this.camera.updateProjectionMatrix();
     this.renderer.setSize(width, height);
   }
-  
-fitToObject(object) {
-    const box = new THREE.Box3().setFromObject(object);
-    const size = box.getSize(new THREE.Vector3());
-    const center = box.getCenter(new THREE.Vector3());
-
-    object.position.x += (object.position.x - center.x);
-    object.position.y += (object.position.y - center.y);
-    object.position.z += (object.position.z - center.z);
-
-    const maxDim = Math.max(size.x, size.y, size.z);
-    const fov = this.camera.fov * (Math.PI / 180);
-    let cameraZ = Math.abs(maxDim / 2 / Math.tan(fov / 2));
-
-    cameraZ *= 1.8;
-
-    this.camera.position.z = cameraZ;
-    this.camera.lookAt(0, 0, 0);
-} 
 }
