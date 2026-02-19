0030372903195fbd48eef6a817887a83bc276fdc
Vigneshwaran Pugazhenthi <204408473+vickyilovemymother@users.noreply.github.com>
2026-02-19 17:27:34 +0530

    Update EnvironmentManager.js

--- vp-configurator/core/EnvironmentManager.js
+++ vp-configurator/core/EnvironmentManager.js
@@ -1,18 +1,26 @@
 import * as THREE from "three";
-import { RGBELoader } from "three/examples/loaders/RGBELoader.js";
+import { RGBELoader } from "three/examples/jsm/loaders/RGBELoader.js";
 
 export class EnvironmentManager {
   constructor(sceneManager) {
     this.sceneManager = sceneManager;
+    this.hdrTexture = null;
   }
 
   async loadHDR(path) {
     const loader = new RGBELoader();
-    const texture = await loader.loadAsync(path);
 
-    texture.mapping = THREE.EquirectangularReflectionMapping;
+    this.hdrTexture = await loader.loadAsync(path);
+    this.hdrTexture.mapping = THREE.EquirectangularReflectionMapping;
 
-    this.sceneManager.scene.environment = texture;
-    this.sceneManager.scene.background = texture;
+    this.sceneManager.scene.environment = this.hdrTexture;
+  }
+
+  setIntensity(value) {
+    this.sceneManager.scene.traverse((child) => {
+      if (child.isMesh && child.material.envMapIntensity !== undefined) {
+        child.material.envMapIntensity = value;
+      }
+    });
   }
 }
