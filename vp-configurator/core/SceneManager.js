d4247d51bfcfa2b3b9351965c468742aded4e21b
Vigneshwaran Pugazhenthi <204408473+vickyilovemymother@users.noreply.github.com>
2026-02-19 16:21:29 +0530

    Update SceneManager.js

--- vp-configurator/core/SceneManager.js
+++ vp-configurator/core/SceneManager.js
@@ -1,10 +1,10 @@
 import * as THREE from "three";
-import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
+import { OrbitControls } from "three/examples/controls/OrbitControls.js";
 
 export class SceneManager {
   constructor() {
-
     this.container = document.getElementById("vp-canvas");
+
     if (!this.container) {
       throw new Error("vp-canvas container not found.");
     }
@@ -22,7 +22,7 @@ export class SceneManager {
 
     this.renderer = new THREE.WebGLRenderer({
       antialias: true,
-      alpha: true
+      alpha: false
     });
 
     this.renderer.setSize(
@@ -31,9 +31,11 @@ export class SceneManager {
     );
 
     this.renderer.setPixelRatio(window.devicePixelRatio);
-
     this.renderer.toneMapping = THREE.ACESFilmicToneMapping;
     this.renderer.outputColorSpace = THREE.SRGBColorSpace;
+
+    this.renderer.shadowMap.enabled = true;
+    this.renderer.shadowMap.type = THREE.PCFSoftShadowMap;
 
     this.container.appendChild(this.renderer.domElement);
 
@@ -52,10 +54,6 @@ export class SceneManager {
     this.scene.add(object);
   }
 
-  remove(object) {
-    this.scene.remove(object);
-  }
-
   start() {
     const animate = () => {
       requestAnimationFrame(animate);
@@ -72,23 +70,5 @@ export class SceneManager {
     this.camera.aspect = width / height;
     this.camera.updateProjectionMatrix();
     this.renderer.setSize(width, height);
-  }
-
-  fitCameraToObject(object) {
-    const box = new THREE.Box3().setFromObject(object);
-    const size = box.getSize(new THREE.Vector3());
-    const center = box.getCenter(new THREE.Vector3());
-
-    const maxDim = Math.max(size.x, size.y, size.z);
-    const fov = this.camera.fov * (Math.PI / 180);
-
-    let cameraZ = Math.abs(maxDim / 2 / Math.tan(fov / 2));
-    cameraZ *= 1.6;
-
-    this.camera.position.set(center.x, center.y, cameraZ);
-    this.camera.lookAt(center);
-
-    this.controls.target.copy(center);
-    this.controls.update();
   }
 }
