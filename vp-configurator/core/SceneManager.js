0e562bb4197e15f5fabaf2aea689a69fdcb10679
Vigneshwaran Pugazhenthi <204408473+vickyilovemymother@users.noreply.github.com>
2026-02-24 01:46:31 +0530

    Update SceneManager.js

--- vp-configurator/core/SceneManager.js
+++ vp-configurator/core/SceneManager.js
@@ -1,26 +1,36 @@
-// Add to SceneManager class
-focusCamera(part) {
-    const targets = {
-        'top': { y: 1.4, distance: 1.5 },
-        'bottom': { y: 0.6, distance: 1.8 },
-        'jacket': { y: 1.2, distance: 2.2 },
-        'comboset': { y: 1.0, distance: 2.5 },
-        'default': { y: 1.0, distance: 3.0 }
-    };
+export class SceneManager {
+    constructor(containerID) {
+        this.container = document.getElementById(containerID);
+        this.scene = new THREE.Scene();
+        this.setupCamera();
+        this.setupRenderer();
+        this.setupControls();
+        this.animate();
+    }
 
-    const target = targets[part] || targets['default'];
+    setupCamera() {
+        this.camera = new THREE.PerspectiveCamera(45, this.container.clientWidth / this.container.clientHeight, 0.1, 1000);
+        this.camera.position.set(0, 1, 3);
+    }
 
-    // Using GSAP for a smooth "Principal R&D" transition
-    gsap.to(this.controls.target, {
-        y: target.y,
-        duration: 1.2,
-        ease: "power2.inOut"
-    });
+    setupRenderer() {
+        this.renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
+        this.renderer.setSize(this.container.clientWidth, this.container.clientHeight);
+        this.renderer.setPixelRatio(window.devicePixelRatio);
+        this.renderer.toneMapping = THREE.ACESFilmicToneMapping;
+        this.renderer.outputEncoding = THREE.sRGBEncoding;
+        this.container.appendChild(this.renderer.domElement);
+    }
 
-    gsap.to(this.camera.position, {
-        z: target.distance,
-        duration: 1.2,
-        ease: "power2.inOut",
-        onUpdate: () => this.controls.update()
-    });
+    setupControls() {
+        this.controls = new THREE.OrbitControls(this.camera, this.renderer.domElement);
+        this.controls.enableDamping = true;
+        this.controls.target.set(0, 1, 0);
+    }
+
+    animate() {
+        requestAnimationFrame(() => this.animate());
+        this.controls.update();
+        this.renderer.render(this.scene, this.camera);
+    }
 }
