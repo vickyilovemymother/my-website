21c5c590c72299f0665aba716100dd51578e2a78
Vigneshwaran Pugazhenthi <204408473+vickyilovemymother@users.noreply.github.com>
2026-02-24 01:51:31 +0530

    Update ModelLoader.js

--- vp-configurator/core/ModelLoader.js
+++ vp-configurator/core/ModelLoader.js
@@ -1,23 +1,26 @@
-import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
-import { DRACOLoader } from "three/examples/jsm/loaders/DRACOLoader.js";
+import * as THREE from 'https://cdn.skypack.dev/three@0.150.0';
+import { GLTFLoader } from 'https://cdn.skypack.dev/three@0.150.0/examples/jsm/loaders/GLTFLoader.js';
+import { DRACOLoader } from 'https://cdn.skypack.dev/three@0.150.0/examples/jsm/loaders/DRACOLoader.js';
 
 export class ModelLoader {
-  constructor() {
-    this.loader = new GLTFLoader();
+    constructor() {
+        this.loader = new GLTFLoader();
+        const dracoLoader = new DRACOLoader();
+        
+        // FIX: Point this to your root directory where draco files are
+        // GitHub Pages requires the absolute relative path
+        dracoLoader.setDecoderPath('./'); 
+        
+        this.loader.setDRACOLoader(dracoLoader);
+    }
 
-    const dracoLoader = new DRACOLoader();
-    dracoLoader.setDecoderPath("/vp-configurator/assets/draco/");
-    this.loader.setDRACOLoader(dracoLoader);
-  }
-
-  async load(path) {
-    return new Promise((resolve, reject) => {
-      this.loader.load(
-        path,
-        (gltf) => resolve(gltf.scene),
-        undefined,
-        (error) => reject(error)
-      );
-    });
-  }
+    async load(path) {
+        return new Promise((resolve, reject) => {
+            this.loader.load(path, 
+                (gltf) => resolve(gltf),
+                (xhr) => { console.log((xhr.loaded / xhr.total * 100) + '% loaded'); },
+                (error) => reject(error)
+            );
+        });
+    }
 }
