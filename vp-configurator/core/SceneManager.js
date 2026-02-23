a002225c86fbf1a1467c80ea0e079187551b7384
Vigneshwaran Pugazhenthi <204408473+vickyilovemymother@users.noreply.github.com>
2026-02-24 01:28:27 +0530

    Update SceneManager.js

--- vp-configurator/core/SceneManager.js
+++ vp-configurator/core/SceneManager.js
@@ -1,91 +1,26 @@
-import * as THREE from "three";
-import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
+// Add to SceneManager class
+focusCamera(part) {
+    const targets = {
+        'top': { y: 1.4, distance: 1.5 },
+        'bottom': { y: 0.6, distance: 1.8 },
+        'jacket': { y: 1.2, distance: 2.2 },
+        'comboset': { y: 1.0, distance: 2.5 },
+        'default': { y: 1.0, distance: 3.0 }
+    };
 
-export class SceneManager {
-  constructor() {
-    this.container = document.getElementById("vp-canvas");
-    if (!this.container) {
-      throw new Error("vp-canvas container not found.");
-    }
+    const target = targets[part] || targets['default'];
 
-    this.scene = new THREE.Scene();
-
-    this.camera = new THREE.PerspectiveCamera(
-      45,
-      this.container.clientWidth / this.container.clientHeight,
-      0.1,
-      1000
-    );
-
-    this.camera.position.set(0, 1.4, 4);
-
-    this.renderer = new THREE.WebGLRenderer({
-      antialias: true,
-      alpha: false,
-      preserveDrawingBuffer: true
+    // Using GSAP for a smooth "Principal R&D" transition
+    gsap.to(this.controls.target, {
+        y: target.y,
+        duration: 1.2,
+        ease: "power2.inOut"
     });
 
-    this.renderer.setSize(
-      this.container.clientWidth,
-      this.container.clientHeight
-    );
-
-    this.renderer.outputColorSpace = THREE.SRGBColorSpace;
-    this.renderer.toneMapping = THREE.ACESFilmicToneMapping;
-
-    this.container.appendChild(this.renderer.domElement);
-
-    this.controls = new OrbitControls(
-      this.camera,
-      this.renderer.domElement
-    );
-
-    this.controls.enableDamping = true;
-    this.controls.dampingFactor = 0.05;
-    this.controls.target.set(0, 1.2, 0);
-
-    window.addEventListener("resize", () => this.onResize());
-  }
-
-  add(object) {
-    this.scene.add(object);
-  }
-
-  remove(object) {
-    this.scene.remove(object);
-  }
-
-  start() {
-    const animate = () => {
-      requestAnimationFrame(animate);
-      this.controls.update();
-      this.renderer.render(this.scene, this.camera);
-    };
-    animate();
-  }
-
-  onResize() {
-    const width = this.container.clientWidth;
-    const height = this.container.clientHeight;
-
-    this.camera.aspect = width / height;
-    this.camera.updateProjectionMatrix();
-    this.renderer.setSize(width, height);
-  }
-
-  fitToObject(object) {
-    const box = new THREE.Box3().setFromObject(object);
-    const size = box.getSize(new THREE.Vector3());
-    const center = box.getCenter(new THREE.Vector3());
-
-    const maxDim = Math.max(size.x, size.y, size.z);
-    const fov = this.camera.fov * (Math.PI / 180);
-
-    let cameraZ = Math.abs(maxDim / 2 / Math.tan(fov / 2));
-    cameraZ *= 1.6;
-
-    this.camera.position.set(center.x, center.y, cameraZ);
-    this.controls.target.copy(center);
-    this.controls.update();
-  }
+    gsap.to(this.camera.position, {
+        z: target.distance,
+        duration: 1.2,
+        ease: "power2.inOut",
+        onUpdate: () => this.controls.update()
+    });
 }
