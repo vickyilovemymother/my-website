e6f01f40683e0359e8cef0f8015dfb36765303d1
Vigneshwaran Pugazhenthi <204408473+vickyilovemymother@users.noreply.github.com>
2026-02-24 01:22:26 +0530

    Update main.js

--- vp-configurator/main.js
+++ vp-configurator/main.js
@@ -1,49 +1,33 @@
-import { SceneManager } from "./core/SceneManager.js";
-import { EnvironmentManager } from "./core/EnvironmentManager.js";
-import { ModelLoader } from "./core/ModelLoader.js";
-import { StateManager } from "./core/StateManager.js";
+// 1. STATE MANAGEMENT
+let config = {
+    gender: 'Men',
+    activeGarments: { top: null, bottom: null, jacket: null, set: null },
+    mannequinVisible: true
+};
 
-import { MannequinController } from "./controllers/MannequinController.js";
-import { GarmentController } from "./controllers/GarmentController.js";
-import { ColorController } from "./controllers/ColorController.js";
-
-const sceneManager = new SceneManager();
-const environmentManager = new EnvironmentManager(sceneManager);
-const modelLoader = new ModelLoader();
-const stateManager = new StateManager();
-
-const mannequinController = new MannequinController(
-  sceneManager,
-  modelLoader,
-  stateManager
-);
-
-const garmentController = new GarmentController(
-  sceneManager,
-  modelLoader,
-  stateManager
-);
-
-const colorController = new ColorController(garmentController);
-
-async function init() {
-  await environmentManager.loadHDR(
-    "/vp-configurator/assets/hdr/HC_VP.hdr"
-  );
-
-  await mannequinController.load("men");
-
-  sceneManager.start();
+// 2. THE 3D LOADER FUNCTION
+function loadGarment(category, fileName) {
+    const path = `assets/clothes/${config.gender}/${category}/${fileName}.glb`;
+    
+    loader.load(path, (gltf) => {
+        // Remove existing item in that category
+        if(config.activeGarments[category]) {
+            scene.remove(config.activeGarments[category]);
+        }
+        
+        // Add new item and save reference
+        const model = gltf.scene;
+        scene.add(model);
+        config.activeGarments[category] = model;
+    });
 }
 
-init();
-
-/* ==== UI API ==== */
-
-window.switchGender = async (gender) => {
-  await mannequinController.load(gender);
-};
-
-window.changeColor = (type, color) => {
-  colorController.change(type, color);
-};
+// 3. COLOR UPDATE FUNCTION
+function changeColor(category, hex) {
+    const model = config.activeGarments[category];
+    if (model) {
+        model.traverse((node) => {
+            if (node.isMesh) node.material.color.set(hex);
+        });
+    }
+}
