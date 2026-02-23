0293146fa55e3c048f454a0211866b6dc24c2c50
Vigneshwaran Pugazhenthi <204408473+vickyilovemymother@users.noreply.github.com>
2026-02-24 01:56:04 +0530

    Update SceneManager.js

--- vp-configurator/core/SceneManager.js
+++ vp-configurator/core/SceneManager.js
@@ -1,16 +1,20 @@
+import * as THREE from 'https://cdn.skypack.dev/three@0.150.0';
+import { OrbitControls } from 'https://cdn.skypack.dev/three@0.150.0/examples/jsm/controls/OrbitControls.js';
+
 export class SceneManager {
-    constructor(containerID) {
-        this.container = document.getElementById(containerID);
+    constructor(containerId) {
+        this.container = document.getElementById(containerId);
         this.scene = new THREE.Scene();
         this.setupCamera();
         this.setupRenderer();
         this.setupControls();
+        window.addEventListener('resize', () => this.onWindowResize());
         this.animate();
     }
 
     setupCamera() {
-        this.camera = new THREE.PerspectiveCamera(45, this.container.clientWidth / this.container.clientHeight, 0.1, 1000);
-        this.camera.position.set(0, 1, 3);
+        this.camera = new THREE.PerspectiveCamera(45, this.container.clientWidth / this.container.clientHeight, 0.1, 100);
+        this.camera.position.set(0, 1.2, 3);
     }
 
     setupRenderer() {
@@ -23,9 +27,15 @@ export class SceneManager {
     }
 
     setupControls() {
-        this.controls = new THREE.OrbitControls(this.camera, this.renderer.domElement);
+        this.controls = new OrbitControls(this.camera, this.renderer.domElement);
         this.controls.enableDamping = true;
         this.controls.target.set(0, 1, 0);
+    }
+
+    onWindowResize() {
+        this.camera.aspect = this.container.clientWidth / this.container.clientHeight;
+        this.camera.updateProjectionMatrix();
+        this.renderer.setSize(this.container.clientWidth, this.container.clientHeight);
     }
 
     animate() {
